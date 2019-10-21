2c8b4396c69e5404e1bb28b9f87eae1f9a0fa14b
sergeydushechkin <54816584+sergeydushechkin@users.noreply.github.com>
2019-10-21 17:06:59 +0300

    Сделал промо слайдер на js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Мольное окно карты
 if (document.querySelector(".modal-map")) {
   var mapmodal = document.querySelector(".modal-map");
   var mapshowbutton = document.querySelector(".button-showmap");
@@ -14,6 +15,7 @@ if (document.querySelector(".modal-map")) {
   });
 }
 
+// МОдальное окно "Напишите нам"
 if (document.querySelector(".modal-writeus")) {
   var writeusmodal = document.querySelector(".modal-writeus");
   var writeusshowbutton = document.querySelector(".button-feedback");
@@ -32,6 +34,7 @@ if (document.querySelector(".modal-writeus")) {
   });
 }
 
+// Модальное окно "Товар добавлен в корзину"
 if (document.querySelector(".modal-cart")) {
   var cartmodal = document.querySelector(".modal-cart");
   var cartshowbutton = document.querySelector(".item-popup-buy");
@@ -54,3 +57,62 @@ if (document.querySelector(".modal-cart")) {
 }
 
 
+// Промо слайдер, работает как от checkbox так и от стрелок(циклически)
+if (document.querySelector(".promo-slider")) {
+  var promoSlide1 = document.querySelector(".promo-slider-slide1");
+  var promoSlide2 = document.querySelector(".promo-slider-slide2");
+  var promoSliderRadio1 = document.querySelector(".promo-slider-radio1");
+  var promoSliderRadio2 = document.querySelector(".promo-slider-radio2");
+  var promoSliderBack = document.querySelector(".promo-slider-back");
+  var promoSliderForward = document.querySelector(".promo-slider-forward");
+
+  promoSliderBack.addEventListener("click", function(event) {
+    event.preventDefault();
+    if (promoSliderRadio1.checked && !promoSliderRadio2.checked) {
+      promoSliderRadio2.checked = true;
+      ShowSlide(2);
+    } else {
+      promoSliderRadio1.checked = true;
+      promoSliderRadio2.checked = false;
+      ShowSlide(1);
+    }
+  });
+
+  promoSliderForward.addEventListener("click", function(event) {
+    event.preventDefault();
+    if (promoSliderRadio1.checked && !promoSliderRadio2.checked) {
+      promoSliderRadio2.checked = true;
+      ShowSlide(2);
+    } else {
+      promoSliderRadio1.checked = true;
+      promoSliderRadio2.checked = false;
+      ShowSlide(1);
+    }
+  });
+
+  promoSliderRadio1.addEventListener("change", function(event) {
+    event.preventDefault();
+    if (event.target.checked) {
+      ShowSlide(1);
+    }
+  });
+
+  promoSliderRadio2.addEventListener("change", function(event) {
+    event.preventDefault();
+    if (event.target.checked) {
+      ShowSlide(2);
+    }
+  });
+
+
+  function ShowSlide(num) {
+    if (num === 1) {
+      promoSlide2.classList.add("promo-slider-hide");
+      promoSlide1.classList.remove("promo-slider-hide");
+    } else {
+      promoSlide1.classList.add("promo-slider-hide");
+      promoSlide2.classList.remove("promo-slider-hide");
+    }
+  }
+
+}
